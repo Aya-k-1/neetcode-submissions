3f5809dfc74602c209546edbf8693c0fe081185a
Aya <ayakhalife01@gmail.com>
2026-08-12 22:40:58 +0300

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length!=t.Length){
+            return false;
+        }
+        int[] frequency = new int[26];
+        for(int i=0;i<s.Length;i++){
+            frequency[s[i]-'a']++;
+            frequency[t[i]-'a']--;
+        }
+        foreach(int element in frequency){
+            if(element!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
