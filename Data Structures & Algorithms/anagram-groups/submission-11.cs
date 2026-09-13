dc5f9fbd859d7120ce7760714a49fc2e2b5b7b20
Aya <ayakhalife01@gmail.com>
2026-09-13 16:31:12 +0300

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+     //   Dictionary<string,string> map = new Dictionary<string,string>();
+        Dictionary<string,List<string>> result= new Dictionary<string,List<string>>();
+        for(int i=0;i<strs.Length;i++){
+            char[] letters = strs[i].ToCharArray();
+            int[] count=new int[26];
+            for(int j=0;j<letters.Length;j++){
+                count[letters[j]-'a']++;
+            }
+           string countString=String.Join(',',count);
+        //    if(!map.ContainsKey(countString)){
+        //     map.Add(countString,strs[i]);
+        //    }
+           if(!result.ContainsKey(countString) ){
+                result.Add(countString,new List<string>() {strs[i]});
+           }else{
+            result[countString].Add(strs[i]);
+           }
+        }
+            return result.Values.ToList();
+        
+    }
+}
