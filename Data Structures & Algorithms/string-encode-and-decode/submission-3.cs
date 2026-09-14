1aafd895677b820adc3b7ef2cc8ea3522f1064ab
Aya <ayakhalife01@gmail.com>
2026-09-14 13:53:45 +0300

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cs
@@ -0,0 +1,29 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        string encoded_string="";
+        for(int i=0;i<strs.Count;i++){
+            encoded_string+=strs[i].Length.ToString()+"#"+strs[i];
+        }
+        return encoded_string;
+        
+    }
+
+    public List<string> Decode(string s) {
+        List<string> decoded_strs = new List<string>();
+        int i=0;
+        while(i<s.Length){
+            int j=i;
+            while(s[j]!='#'){
+                j++;
+            }
+            int length= int.Parse(s.Substring(i,j-i));
+            string word= s.Substring(j+1,length);
+            decoded_strs.Add(word);
+            i=j+1+length;
+
+        }
+        return decoded_strs;
+   }
+
+}
